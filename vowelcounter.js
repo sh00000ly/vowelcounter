ef583d8e014f9b0924d3a95e1497c1f78978e6e8
sheila <sheiladoesart@gmail.com>
2024-06-29 09:52:53 -0300

    added vowel counter function

---
+++ vowelcounter.js
@@ -0,0 +1,23 @@
+// This code will count the number of vowels in a given word.
+
+function vowelCounter(word) {
+    let i = 0;
+    let vowelCounter = 0;
+    while (i < word.length) {
+        let letter = word[i];
+        if (letter === "a" ||
+            letter === "e" ||
+            letter === "i" ||
+            letter === "o" ||
+            letter === "u") {
+                vowelCounter++;
+            }
+            i++;
+    }
+    return vowelCounter;
+
+}
+
+console.log(vowelCounter("imagination"));
+console.log(vowelCounter("omelette"));
+console.log(vowelCounter("egg"));
